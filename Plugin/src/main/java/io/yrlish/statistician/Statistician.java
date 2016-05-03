b99303b7410ace6c8c60d431f09397abc0641db4
Dennis Yrlish <me@yrlish.io>
2016-05-03 19:50:59 +0200

    Initial commit.

---
+++ Plugin/src/main/java/io/yrlish/statistician/Statistician.java
@@ -0,0 +1,66 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Dennis Alexandersson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.yrlish.statistician;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+import java.nio.file.Path;
+
+@Plugin(id = "statistician", name = "Statistician", version = "0.1.0")
+public class Statistician {
+    @Inject
+    private static Logger logger;
+
+    @Inject
+    @ConfigDir(sharedRoot = false)
+    private Path configDir;
+
+    private static Statistician instance;
+
+    private Game game;
+
+    @Listener
+    public void onServerInit(GameInitializationEvent event) {
+        getLogger().info("Statistician is loading...");
+        game = Sponge.getGame();
+
+        // Init commands
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    public static Statistician getInstance() {
+        return instance;
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Statistician
+The Statistician who keeps track of your server and its users.
+
+## Requirements
+* [Sponge](https://spongepowered.org)
+
+## Features
+### Current
+* TODO
+
+### Planned
+* Log server uptime, current players, top max players (month/ever)
+* Log player total online time, time since logged in, time when logged off
