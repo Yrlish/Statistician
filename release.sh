851261e56769b66ccce6316ebf39af36a0141435
Dennis Yrlish <me@yrlish.io>
2016-09-15 22:00:56 +0200

    Updated release script for preparations when to ship webpanel

--- release.sh
+++ release.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
 CURRENT_DIR=$(pwd)
-BUILD_NUMBER=${BUILD_NUMBER}
+WORKING_DIR=$(mktemp -d)
 
-####################
-## PLUGIN RELEASE ##
-####################
 
-cd ${CURRENT_DIR}/Plugin
+##
+## Prepare packaging artifact
+##
 
-# Rename jar file to include the build number
-rename 's/DEV_BUILD/${BUILD_NUMBER}/' target/*.jar
+mkdir -p ${WORKING_DIR}/Plugin
+mkdir -p ${WORKING_DIR}/Webpanel
+
+
+##
+## Plugin
+##
+
+cp -f ${CURRENT_DIR}/Plugin/target/Statistician-*.jar ${WORKING_DIR}/Plugin/
+
+
+##
+## Webportal
+##
+
+
+
+##
+## Package artifact
+##
+
+cd ${WORKING_DIR}
+zip -r ${CURRENT_DIR}/Statistician-${POM_VERSION}-${BUILD_NUMBER}.zip *
+
